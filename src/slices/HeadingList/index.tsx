a9bb6e4ceaf5f5e8077a6bca21f66db99d7a759c
Marius Jopen <mail@mariusjopen.com>
2025-11-11 10:52:34 +0100

    fade in box

--- src/slices/HeadingList/index.tsx
+++ src/slices/HeadingList/index.tsx
@@ -4,8 +4,7 @@ import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import { PrismicRichText } from "@prismicio/react";
-import { StaggerContainer, FadeInUp, fadeInUpVariants } from "@/lib/FramerStagger";
-import { motion } from "framer-motion";
+import { StaggerContainer, FadeInUp } from "@/lib/FramerStagger";
 
 /**
  * Props for `HeadingList`.
@@ -21,39 +20,33 @@ const HeadingList: FC<HeadingListProps> = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <motion.div
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0.05 }}
-        variants={fadeInUpVariants}
-        className="bg-primary rounded-lg pt-8 mx-auto w-3/5 mb-12"
-      >
-        <StaggerContainer>
-          <FadeInUp>
-            <div className="text-center pb-6">
-              {slice.primary.title && (
-                <div className="uppercase font-medium">
-                  <PrismicRichText field={slice.primary.title} />
-                </div>
-              )}
-            </div>
-          </FadeInUp>
-          
-          {slice.primary.items && slice.primary.items.length > 0 && (
-            <ul className="flex flex-col justify-center gap-4 pb-10">
-              {slice.primary.items.map((item, index) => (
-                <FadeInUp key={index}>
-                  <div className="w-full flex justify-center">
-                    <li className="list-disc">
-                      {item.text}
-                    </li>
+      <StaggerContainer className="mx-auto w-3/5 mb-12" retriggerOnPathname>
+        <FadeInUp className="rounded-lg bg-primary pt-8">
+          <div>
+            <FadeInUp>
+              <div className="text-center pb-6">
+                {slice.primary.title && (
+                  <div className="uppercase font-medium">
+                    <PrismicRichText field={slice.primary.title} />
                   </div>
-                </FadeInUp>
-              ))}
-            </ul>
-          )}
-        </StaggerContainer>
-      </motion.div>
+                )}
+              </div>
+            </FadeInUp>
+
+            {slice.primary.items && slice.primary.items.length > 0 && (
+              <ul className="flex flex-col justify-center gap-4 pb-10">
+                {slice.primary.items.map((item, index) => (
+                  <FadeInUp key={index}>
+                    <div className="w-full flex justify-center">
+                      <li className="list-disc">{item.text}</li>
+                    </div>
+                  </FadeInUp>
+                ))}
+              </ul>
+            )}
+          </div>
+        </FadeInUp>
+      </StaggerContainer>
     </section>
   );
 };
